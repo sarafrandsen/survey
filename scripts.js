8f6566015cab3e5204ae3808314dcc4fac2d5050
Sara Frandsen <sara.frandsen@gmail.com>
2017-01-16 18:43:07 -0800

    create survey and display results

--- scripts.js
+++ scripts.js
@@ -2,18 +2,22 @@ $(document).ready(function() {
   $("#survey form").submit(function(event) {
     var nameInput = $("input#name").val();
     var ageInput = $("input#age").val();
+    var birthdayInput = $("#birthday").val();
+
     var foodInput = $("input#food").val();
     var musicInput = $("input#music").val();
     var movieInput = $("input#movie").val();
-    var dogsvcatsInput = $("input#dogs-v-cats").val();
+    var dogsCatsChecked = $("input:radio[name=dogs-v-cats]:checked").val();
 
     $(".name").text(nameInput);
     $(".age").text(ageInput);
+    $(".birthday").text(birthdayInput);
+
     $(".food").text(foodInput);
     $(".music").text(musicInput);
     $(".movie").text(movieInput);
-//???????????????????????????????????
-    $(".dogs-v-cats").text(dogsvcatsInput);
+    $(".dogs-v-cats").text(dogsCatsChecked);
+// //???????????????????????????????????
 
     $("#results").show();
 
